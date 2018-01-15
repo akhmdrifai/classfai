ac7daed871dd73bdaa8ea9832743caaade1394f6
akhmdrifai <32448266+akhmdrifai@users.noreply.github.com>
2018-01-15 20:59:16 +0800

    menambahkan nilai string

--- classfai.cpp
+++ classfai.cpp
@@ -10,5 +10,16 @@ class ikan{
 int main(){
 	ikan kakap, nila, bawal;
 
+	kakap.panjang = "Panjangnya 60cm ";
+	nila.panjang = "Panjangnya 40cm ";
+	bawal.panjang = "Panjangnya 50cm ";
 	
-}
+	kakap.massa = "Massanya 1kg ";
+	nila.massa = "Massanya 1kg ";
+	bawal.massa = "Massanya 1,5kg ";
+	
+	kakap.warna = "Warnanya merah ";
+	nila.warna = "Warna hitam ";
+	bawal.warna = "Warnanya hitam ";
+	
+};
