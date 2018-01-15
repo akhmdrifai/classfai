ba99155ddb47321cfafeb941abcabcf907f74715
akhmdrifai <32448266+akhmdrifai@users.noreply.github.com>
2018-01-15 20:54:28 +0800

    membuat class ikan

--- classfai.cpp
+++ classfai.cpp
@@ -1,6 +1,13 @@
  #include <iostream>
 #include <string>
 using namespace std;
+class ikan{
+	public:
+		void habitat(string habitat);
+		string panjang, massa, warna;
+		
+};
+
 int main(){
 	
-};
+}
